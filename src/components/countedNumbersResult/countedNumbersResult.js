09964e5c3117a11c3722ee923eb4921661ea7419
Alisa <mme.figaro@ya.ru>
2021-07-20 19:51:05 +0300

    component for numbers

--- src/components/countedNumbersResult/countedNumbersResult.js
+++ src/components/countedNumbersResult/countedNumbersResult.js
@@ -1,10 +1,23 @@
 import { useState, useEffect } from "react";
 
 const CountedNumbersResult = (props) => {
-  
+  const [proizv, setProizv] = useState(null);
+
+  useEffect(() => {
+    if (props.selectedNumbers && props.selectedNumbers.length) {
+      let proizvTemp = 1;
+      for (let k = 0; k < props.selectedNumbers.length; k++) {
+        proizvTemp *= +props.selectedNumbers[k].value;
+      }
+      setProizv(proizvTemp.toFixed());
+    } else {
+      setProizv(null);
+    }
+  }, [props.selectedNumbers]);
+
   return (
     <div>
-          Произведение чисел: 
+          Произведение чисел: {proizv ? proizv : 'пока нет результата.'}
     </div>
   )
 }
