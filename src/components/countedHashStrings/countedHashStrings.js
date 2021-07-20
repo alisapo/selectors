389460d4c3e64dc913cb13178eefc70de30c7af5
Alisa <mme.figaro@ya.ru>
2021-07-20 19:51:53 +0300

    component for strings

--- src/components/countedHashStrings/countedHashStrings.js
+++ src/components/countedHashStrings/countedHashStrings.js
@@ -1,10 +1,23 @@
 import { useState, useEffect } from "react";
 
 const CountedHashStrings = (props) => {
+  const [hashString, setHashString] = useState(null);
+
+  useEffect(() => {
+    if (props.selectedStrings && props.selectedStrings.length) {
+      let newString = '';
+      for (let i = 0; i < props.selectedStrings.length; i++) {
+        newString += props.selectedStrings[i].value;
+      }
+      setHashString(newString);
+    } else {
+      setHashString(null);
+    }
+  }, [props.selectedStrings]);
 
   return (
     <div>
-      Хэш строки: 
+      Хэш строки: {hashString ? hashString : 'пока нет результата.'}
     </div>
   )
 }
