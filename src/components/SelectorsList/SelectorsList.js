3da59bc9052fba4363372e5667a23954c255ed90
Alisa <mme.figaro@ya.ru>
2021-07-27 12:53:41 +0300

    cut SelectorsList

--- src/components/SelectorsList/SelectorsList.js
+++ src/components/SelectorsList/SelectorsList.js
@@ -8,31 +8,24 @@ const SelectorsList = (props) => {
     {name: 'Объекты', selectors: props.objects}
   ];
 
-  const selector = (arr, name) => {
-    return (
-      arr.length ?
-        <div className="selected">
-          <p className="selected-header">{name}: </p>
-          {arr.map(item => {
-            return (
-              <div key={item.value ? item.value.toString() : item.key}>
-                {typeof item.value === 'boolean' ?
-                   (item.value === true ? 'true' : 'false')
-                   : item.value}
-              </div>
-          )})}
-        </div> : <div className='no-selected'>Пока нет выбранных элементов.</div>
-    )
-  }
-
   return (
     <div className="selector-list">
       <div className="section-header">Выбранные значения: </div>
       {data.map(obj => {
         return (
-          selector(obj.selectors, obj.name)
-        )
-      })}
+          obj.selectors.length ?
+            <div className="selected">
+              <p className="selected-header">{obj.name}: </p>
+              {obj.selectors.map(item => {
+                return (
+                  <div key={item.value ? item.value.toString() : item.key}>
+                    {typeof item.value === 'boolean' ?
+                      (item.value === true ? 'true' : 'false')
+                      : item.value}
+                  </div>
+                )})}
+            </div> : <div className='no-selected'>Пока нет выбранных элементов.</div>
+        )})}
     </div>
   )
 }
