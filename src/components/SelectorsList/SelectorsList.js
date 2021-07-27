3799bab24e985cfb5d888fe725f02eba04218cb5
Alisa <mme.figaro@ya.ru>
2021-07-27 12:49:37 +0300

    cut SelectorsList

--- src/components/SelectorsList/SelectorsList.js
+++ src/components/SelectorsList/SelectorsList.js
@@ -1,52 +1,38 @@
 import './selectorsList.scss';
 
 const SelectorsList = (props) => {
-  const numbers = props.numbers,
-    strings = props.strings,
-    booleans = props.booleans,
-    objects = props.objects;
+  const data = [
+    {name: 'Числа', selectors: props.numbers},
+    {name: 'Строки', selectors: props.strings},
+    {name: 'Логические значения', selectors: props.booleans},
+    {name: 'Объекты', selectors: props.objects}
+  ];
+
+  const selector = (arr, name) => {
+    return (
+      arr.length ?
+        <div className="selected">
+          <p className="selected-header">{name}: </p>
+          {arr.map(item => {
+            return (
+              <div key={item.value ? item.value.toString() : item.key}>
+                {typeof item.value === 'boolean' ?
+                   (item.value === true ? 'true' : 'false')
+                   : item.value}
+              </div>
+          )})}
+        </div> : <div className='no-selected'>Пока нет выбранных элементов.</div>
+    )
+  }
 
   return (
     <div className="selector-list">
       <div className="section-header">Выбранные значения: </div>
-      {numbers.length ?
-        <div className="selected">
-          <p className="selected-header">Числа: </p>
-          {numbers.map(number => {
-            return (
-              <div key={number.value}>{number.value}</div>
-            )
-          })}
-        </div> : <div className='no-selected'>Пока нет выбранных чисел.</div>}
-      {strings.length ?
-        <div className="selected">
-          <p className="selected-header">Строки: </p>
-          {strings.map(string => {
-            return (
-              <div key={string.value}>{string.value}</div>
-            )
-          })}
-        </div> : <div className='no-selected'>Пока нет выбранных строк.</div>}
-      {objects.length ?
-        <div className="selected">
-          <p className="selected-header">Объекты: </p>
-          {objects.map(object => {
-            return (
-              <div key={object.key}>{object.value}</div>
-            )
-          })}
-        </div> : <div className='no-selected'>Пока нет выбранных объектов.</div>}
-      {booleans.length ?
-        <div className="selected">
-          <p className="selected-header">Логические значения: </p>
-          {booleans.map(bool => {
-            return (
-              <div key={bool.value.toString()}>
-                {bool.value === true ? 'true' : 'false'}
-              </div>
-            )
-          })}
-        </div> : <div className='no-selected'>Пока нет выбранных логических значений.</div>}
+      {data.map(obj => {
+        return (
+          selector(obj.selectors, obj.name)
+        )
+      })}
     </div>
   )
 }
